a554886c73a03d484223d580e30ec141243b8202
Arthur Zeni <zeniarthur@gmail.com>
2026-09-09 14:02:29 -0400

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,18 @@
-<h1>Welcome to the Home Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        {{ config('app.name') }}
+    </title>
 
-<p>
-    ola {{ $name }}!
-
-</p>
-
-<p>
-    Seus habitos sao:
-</p>
-<ul>
-    @foreach ($habits as $item)
-        <li>{{ $item }}</li>
-    @endforeach
-</ul>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-blue-500">
+        Hello World!
+    </h1>
+</body>
+</html>
